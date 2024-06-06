f92370e9f7f8e915f6a4bf969c511c72c1e95a4d
vatsal <vso.opash@gmail.com>
2024-06-06 12:49:01 +0530

    initial commit

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,25 @@
+const loginUrl = 'https://stripper.social/login';
+const logoutUrl = 'https://stripper.social/logout';
+const frontendUrl = 'https://tube.stripper.social/';
+const backendUrl =  'https://api.stripper.social/';
+
+// const frontendUrl = 'http://localhost:4200/';
+// const backendUrl = 'http://localhost:8080/';
+// const loginUrl = 'http://localhost:4200/login';
+const wasabiUrl = 'https://organic-dating.s3.wasabisys.com/'
+
+
+export const environment = {
+  production: false,
+  frontendUrl: frontendUrl,
+  backendUrl: backendUrl,
+  loginUrl: loginUrl,
+  apiUrl: `${backendUrl}api/v1/`,
+  domain: '.stripper.social',
+  wasabiUrl: wasabiUrl,
+  socketUrl: `${backendUrl}`,
+  conferenceUrl: 'https://facetime.tube/',
+  logoutUrl: logoutUrl
+
+};
+
